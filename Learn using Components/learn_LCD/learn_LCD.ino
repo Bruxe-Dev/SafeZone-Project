70a4f2bd098189618f5845fd991e4a6842714391
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-24 16:18:46 +0200

    modified it so that it comes like it is being being written now going to move on ot an improved version

--- Learn using Components/learn_LCD/learn_LCD.ino
+++ Learn using Components/learn_LCD/learn_LCD.ino
@@ -4,10 +4,43 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 
 void setup(){
   lcd.begin(16, 2);
-  lcd.print("Danger detected");
+  lcd.print("H");
+  delay(200);
+  lcd.print("E");
+  delay(200);
+  lcd.print("L");
+  delay(200);
+  lcd.print("L");
+  delay(200);
+  lcd.print("O, ");
+  delay(200);
+  lcd.print("H");
+  delay(200);
+  lcd.print("O");
+  delay(200);
+  lcd.print("W ");
+  delay(200);
+  lcd.print("I");
+  delay(200);
+  lcd.print("S ");
+  delay(200);
 
   lcd.setCursor(0, 1);
-  lcd.print("Move away!");
+  lcd.print("E");
+  delay(200);
+  lcd.print("V");
+  delay(200);
+  lcd.print("E");
+  delay(200);
+  lcd.print("R");
+  delay(200);
+  lcd.print("Y");
+  delay(200);
+  lcd.print("O");
+  delay(200);
+  lcd.print("N");
+  delay(200);
+  lcd.print("E");
 }
 void loop(){
 }
