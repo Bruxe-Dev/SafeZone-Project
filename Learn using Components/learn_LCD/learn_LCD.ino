fd377c1b54118ad2e82326aa83cc0ab8d2c46a70
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-24 16:30:52 +0200

    udated the typing effect by using Loop so that i can have cleaner and nicer codes

--- Learn using Components/learn_LCD/learn_LCD.ino
+++ Learn using Components/learn_LCD/learn_LCD.ino
@@ -4,43 +4,13 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 
 void setup(){
   lcd.begin(16, 2);
-  lcd.print("H");
-  delay(200);
-  lcd.print("E");
-  delay(200);
-  lcd.print("L");
-  delay(200);
-  lcd.print("L");
-  delay(200);
-  lcd.print("O, ");
-  delay(200);
-  lcd.print("H");
-  delay(200);
-  lcd.print("O");
-  delay(200);
-  lcd.print("W ");
-  delay(200);
-  lcd.print("I");
-  delay(200);
-  lcd.print("S ");
-  delay(200);
+  lcd.clear();
 
-  lcd.setCursor(0, 1);
-  lcd.print("E");
-  delay(200);
-  lcd.print("V");
-  delay(200);
-  lcd.print("E");
-  delay(200);
-  lcd.print("R");
-  delay(200);
-  lcd.print("Y");
-  delay(200);
-  lcd.print("O");
-  delay(200);
-  lcd.print("N");
-  delay(200);
-  lcd.print("E");
+  char text[]="LCD TEST MODE...";
+  for(int i=0;text[i] !='\0'; i++){
+    lcd.print(text[i]);
+    delay(200);
+  }
 }
 void loop(){
 }
