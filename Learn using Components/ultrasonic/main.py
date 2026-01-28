0a70e848b6a9d3de6161113945a73603abb3d88c
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-28 18:17:11 +0200

    mdified and testedthe codes to the appropriate ranges so that the beeping  sound feels more realistic

--- Learn using Components/ultrasonic/main.py
+++ Learn using Components/ultrasonic/main.py
@@ -1,20 +1,11 @@
 import serial
 import winsound
-import time
 
 arduino = serial.Serial('COM6', 9600)
-time.sleep(2)
 
 while True:
     data = arduino.readline().decode().strip()
     print(data)
 
-    if data.isdigit():
-        distance = int(data)
-
-        if distance <= 30:
-            # Map distance to delay (closer = faster)
-            delay = max(0.05, distance / 100)
-
-            winsound.Beep(1000, 100)
-            time.sleep(delay)
+    if data == "ALERT":
+        winsound.Beep(1400, 790)  # frequency, duration
--- Learn using Components/ultrasonic/ultrasonic.ino
+++ Learn using Components/ultrasonic/ultrasonic.ino
@@ -1,30 +1,46 @@
 #define TRIG_PIN 9
 #define ECHO_PIN 10
+#define RED_LED 2
+#define GREEN_LED 3
 
 long duration;
 int distance;
+int threshold = 20; // cm
 
 void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  // Send ultrasonic pulse
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
+  // Read echo
   duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  distance = duration * 0.034 / 2;
 
-  if (duration == 0) {
-    Serial.println("OUT");
+  if (distance > 0 && distance <= threshold) {
+    Serial.println("ALERT"); 
+     digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    delay(100);
+     digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    delay(100); 
   } else {
-    distance = duration * 0.034 / 2;
-    Serial.println(distance); // send distance
+    Serial.println("CLEAR");  
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+     // Object far
   }
 
-  delay(100);
+  delay(200);
 }
