415aeb1401a5955bfeba6a33f7004de39f564777
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-28 17:30:34 +0200

    added a main.py file to use to make a computer sound when there is an alert to the serial monitor

--- Learn using Components/ultrasonic/ultrasonic.ino
+++ Learn using Components/ultrasonic/ultrasonic.ino
@@ -1,21 +1,18 @@
 #define TRIG_PIN 9
 #define ECHO_PIN 10
-#define RED_LED 2
-#define GREEN_LED 3
 
 long duration;
 int distance;
+int threshold = 70; // cm
 
 void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // Send pulse
+  // Send ultrasonic pulse
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -23,22 +20,13 @@ void loop() {
   digitalWrite(TRIG_PIN, LOW);
 
   // Read echo
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.034 / 2; // Convert to cm
+  duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  distance = duration * 0.034 / 2;
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
-
-  // LED logic
-  if (distance <= 10) { 
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-    delay(50),
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, LOW);
-  } else { // obstacle far
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
+  if (distance > 0 && distance <= threshold) {
+    Serial.println("ALERT");   // Object close
+  } else {
+    Serial.println("CLEAR");   // Object far
   }
 
   delay(200);
