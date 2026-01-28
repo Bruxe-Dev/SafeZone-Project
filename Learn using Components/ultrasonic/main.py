7ed15c2343f24d6d11eba7dd89b34189d04ee16c
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-28 17:42:34 +0200

    updated both the python codes and arduino codes so that it sounds more realistic

--- Learn using Components/ultrasonic/main.py
+++ Learn using Components/ultrasonic/main.py
@@ -1,11 +1,20 @@
 import serial
 import winsound
+import time
 
 arduino = serial.Serial('COM6', 9600)
+time.sleep(2)
 
 while True:
     data = arduino.readline().decode().strip()
     print(data)
 
-    if data == "ALERT":
-        winsound.Beep(1000, 300)  # frequency, duration
+    if data.isdigit():
+        distance = int(data)
+
+        if distance <= 30:
+            # Map distance to delay (closer = faster)
+            delay = max(0.05, distance / 100)
+
+            winsound.Beep(1000, 100)
+            time.sleep(delay)
--- Learn using Components/ultrasonic/ultrasonic.ino
+++ Learn using Components/ultrasonic/ultrasonic.ino
@@ -1,46 +1,30 @@
 #define TRIG_PIN 9
 #define ECHO_PIN 10
-#define RED_LED 2
-#define GREEN_LED 3
 
 long duration;
 int distance;
-int threshold = 70; // cm
 
 void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // Send ultrasonic pulse
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  // Read echo
   duration = pulseIn(ECHO_PIN, HIGH, 30000);
-  distance = duration * 0.034 / 2;
 
-  if (distance > 0 && distance <= threshold) {
-    Serial.println("ALERT"); 
-     digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-    delay(100);
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-    delay(100); 
+  if (duration == 0) {
+    Serial.println("OUT");
   } else {
-    Serial.println("CLEAR");  
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-     // Object far
+    distance = duration * 0.034 / 2;
+    Serial.println(distance); // send distance
   }
 
-  delay(200);
+  delay(100);
 }
