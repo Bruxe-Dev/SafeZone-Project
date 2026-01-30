280c4a006a373addb11535cccc65f39a11f4d4f6
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-30 17:24:59 +0200

    finally found the most accurate frequencies and the duration  and it sounds more realistic now

--- Learn using Components/ultrasonic/main.py
+++ Learn using Components/ultrasonic/main.py
@@ -9,14 +9,14 @@ while True:
     print(data)
 
     if data == "ALERT1":
-        frequency = 1500  # Hz
+        frequency = 1700  # Hz
         duration = 500    # ms
         winsound.Beep(frequency, duration)
         time.sleep(0.1)  
 
     elif data == "ALERT2":
-        frequency = 2500  
-        duration = 150   
-        for _ in range(4):
+        frequency = 1700  
+        duration = 250   
+        for _ in range(40):
             winsound.Beep(frequency, duration)
             time.sleep(0.05)  # very short pause between beeps
